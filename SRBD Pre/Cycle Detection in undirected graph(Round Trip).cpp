b4be62849686e53e0764506fc1bcdd0640a51b57
Maksud Alam Rony <53002641+immaksudalam@users.noreply.github.com>
2022-04-29 22:11:39 -0700

    Update Cycle Detection in undirected graph(Round Trip).cpp

--- SRBD Pre/Cycle Detection in undirected graph(Round Trip).cpp
+++ SRBD Pre/Cycle Detection in undirected graph(Round Trip).cpp
@@ -1,67 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int n, m;
-int start, finish;
 vector<vector<int>>graph;
+vector<int>p;
 vector<int>vis;
-vector<int>parent;
-
-bool dfs(int src, int p){
-    vis[src] = 1;
-    parent[src] = p;
-
-    for(auto it:graph[src]){
-        if(!vis[it]){
-            if(dfs(it, src)) return true;
+ 
+void dfs(int node){
+    vis[node] = 1;
+    for(auto child: graph[node]){
+        if(vis[child] == 0){
+            p[child] = node;
+            dfs(child);
         }
-        else if(it != p){
-            start = it;
-            finish = src;
-            return true;
-        }
-    }
-    return false;
-}
-bool isCycle()
-{
-    for(int i=1; i<=n; i++){
-        if(!vis[i]){
-            if(dfs(i, -1)) return true;
+        else if(child != p[node]){
+            vector<int>path;
+            p[child] = -1;
+            path.push_back(child);
+            while(p[node] != -1){
+                path.push_back(node);
+                node = p[node];
+            }
+            path.push_back(node);
+            reverse(path.begin(), path.end());
+            cout<<path.size()<<endl;
+            for(auto it:path)cout<<it<<" ";
+            exit(0);
         }
     }
-    return false;
 }
 int main()
 {
-    cin>>n>>m;
-    graph.resize(n+1);
-    vis.resize(n+1);
-    parent.resize(n+1);
-
-    for(int i=0; i<m; i++){
-        int a, b;
-        cin>>a>>b;
-        graph[a].push_back(b);
-        graph[b].push_back(a);
-    }
-    bool ans = isCycle();
-    if(!ans){
-        cout<<"IMPOSSIBLE"<<endl;
-    }
-    else{
-        vector<int>ans;
-        int temp = finish;
-        ans.push_back(finish);
-        while(temp != start){
-            ans.push_back(parent[temp]);
-            temp = parent[temp];
-        }
-        ans.push_back(finish);
-        cout<<ans.size()<<endl;
-        for(auto it:ans)cout<<it<<" ";
-        cout<<endl;
-    }
-
-
+   int n, m;
+   cin>>n>>m;
+   graph.resize(n+1);
+   p.resize(n+1);
+   vis.resize(n+1);
+   for(int i = 0; i<m; i++){
+       int u, v;
+       cin>>u>>v;
+       graph[u].push_back(v);
+       graph[v].push_back(u);
+   }
+   for(int i=1; i<=n; i++){
+       if(vis[i])continue;
+       dfs(i);
+   }
+   cout<<"IMPOSSIBLE"<<endl;
 }
