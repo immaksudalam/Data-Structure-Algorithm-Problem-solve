288a2b7a363e6c570d99032a8b9ebcf918887524
Maksud Alam Rony <53002641+immaksudalam@users.noreply.github.com>
2022-04-29 22:07:58 -0700

    Update Find Cycle in Directed Graph (Road Trip 2).cpp

--- SRBD Pre/Find Cycle in Directed Graph (Road Trip 2).cpp
+++ SRBD Pre/Find Cycle in Directed Graph (Road Trip 2).cpp
@@ -3,63 +3,47 @@ using namespace std;
 int n, m;
 vector<vector<int>>graph;
 vector<int>vis;
-stack<int>recursionStack;
-vector<bool>rsFlag;
-
-bool dfs(int src){
-    vis[src] = 1;
-    recursionStack.push(src);
-    rsFlag[src] = 1;
-
-    for(auto it:graph[src]){
-        if(!vis[it]){
-            if(dfs(it)) return true;
+vector<int>parent;
+ 
+void dfs(int node){
+    vis[node]=1;
+    for(auto child:graph[node]){
+        if(vis[child]==0){
+            parent[child]=node;
+            dfs(child);
         }
-        if(rsFlag[it]){
-            recursionStack.push(it);
-            return true;
-        }
-    }
-    recursionStack.pop();
-    rsFlag[src] = false;
-
-    return false;
-}
-void isCycle(){
-    for(int i=1; i<=n; i++){
-        if(!vis[i]){
-            if(dfs(i)) break;
+        else if(vis[child]==1){
+            vector<int>path;
+            parent[child]=-1;
+            path.push_back(child);
+            while(parent[node]!=-1){
+                path.push_back(node);
+                node = parent[node];
+            }
+            path.push_back(node);
+            reverse(path.begin(), path.end());
+            cout<<path.size()<<endl;
+            for(auto it:path)cout<<it<<" ";
+            exit(0);
         }
     }
+    vis[node]=2;
 }
 int main()
 {
-    cin>>n>>m;
-    graph.resize(n+1);
-    vis.resize(n+1);
-    rsFlag.resize(n+1);
-    for(int i=0; i<m; i++){
-        int a, b;
-        cin>>a>>b;
-        graph[a].push_back(b);
-    }
-    isCycle();
-
-    if(recursionStack.empty()){
-        cout<<"IMPOSSIBLE"<<endl;
-    }
-    else{
-        vector<int>ans;
-        int temp = recursionStack.top();
-        while(!recursionStack.empty()){
-            ans.push_back(recursionStack.top());
-            recursionStack.pop();
-            if(ans.back() == temp && ans.size() != 1) 
-                break;
-        }
-        reverse(ans.begin(), ans.end());
-        cout<<ans.size()<<endl;
-        for(auto it:ans) cout<<it<<" ";
-        cout<<endl;
-    }
+   cin>>n>>m;
+   graph.resize(n+1);
+   vis.resize(n+1);
+   parent.resize(n+1);
+   
+   for(int i=0; i<m; i++){
+       int u, v;
+       cin>>u>>v;
+       graph[u].push_back(v);
+   }
+   for(int i=1; i<=n; i++){
+       if(vis[i])continue;
+       dfs(i);
+   }
+   cout<<"IMPOSSIBLE"<<endl;
 }
