9396752502744f44f7ebe0975fdb238b1de5c7d2
Maksud Alam Rony <53002641+immaksudalam@users.noreply.github.com>
2022-05-02 01:25:47 -0700

    Update Unique binary search tree.cpp

--- SRBD Pre/greek for greeks/Unique binary search tree.cpp
+++ SRBD Pre/greek for greeks/Unique binary search tree.cpp
@@ -1,16 +1,19 @@
-class Solution {
-public:
-    int solve(int n, vector<int>&dp){
+class Solution
+{
+    public:
+    int mod = 1e9+7;
+    long long solve(int n, vector<long long>&dp){
         if(n<=1) return 1;
         if(dp[n] != -1) return dp[n];
-        int total = 0;
+        long long total = 0;
         for(int i=1; i<=n; i++){
-            total += solve(i-1, dp)*solve(n-i, dp);
+            total = (total%mod) + ((solve(i-1, dp)%mod)*solve(n-i, dp)%mod)%mod;
+            total%=mod;
         }
         return dp[n] = total;
     }
-    int numTrees(int n) {
-       vector<int>dp(200, -1);
-       return solve(n, dp);
+    int numTrees(int n) 
+    {
+        vector<long long>dp(n+2, -1);
+        return solve(n, dp);
     }
-};
